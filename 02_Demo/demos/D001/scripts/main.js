d871942b65091fe9b004c08c66425d397fcb68c9
githubUser3454321 <116742137+githubUser3454321@users.noreply.github.com>
2026-03-01 18:48:23 +0100

    removed unused code

--- 02_Demo/demos/D001/scripts/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-let name = "IBW-Dozent";
-
-function init() {
-//    document.getElementById("output").innerHTML = "<p>Hallo " + name + "!</p>";
-    document.getElementById("output").innerHTML = `<p>Hallo ${name}!</p>`;
-}
